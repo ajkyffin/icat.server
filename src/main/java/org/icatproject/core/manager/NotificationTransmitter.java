5dd48116473e26a3e1eb1bb6008e70d37099e64e
Alan Kyffin <alan.kyffin@stfc.ac.uk>
2024-11-15 16:57:16 +0000

    Convert to JMS simplified API

--- src/main/java/org/icatproject/core/manager/NotificationTransmitter.java
+++ src/main/java/org/icatproject/core/manager/NotificationTransmitter.java
@@ -1,84 +1,42 @@
 package org.icatproject.core.manager;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
 import jakarta.jms.JMSException;
-import jakarta.jms.MessageProducer;
 import jakarta.jms.ObjectMessage;
-import jakarta.jms.Session;
 import jakarta.jms.Topic;
-import jakarta.jms.TopicConnection;
-import jakarta.jms.TopicConnectionFactory;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.icatproject.core.manager.NotificationMessage.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
 @ApplicationScoped
 public class NotificationTransmitter {
 
-	private static Logger logger = LoggerFactory.getLogger(NotificationTransmitter.class);
-	private final static Marker fatal = MarkerFactory.getMarker("FATAL");
+	private static final Logger logger = LoggerFactory.getLogger(NotificationTransmitter.class);
 
-	private Topic topic;
+	@Resource
+	ConnectionFactory connectionFactory;
 
-	@Inject
-	PropertyHandler propertyHandler;
-
-	private TopicConnection topicConnection;
-
-	@PostConstruct
-	void init() {
-
-		try {
-			InitialContext ic = new InitialContext();
-			TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory) ic
-					.lookup(propertyHandler.getJmsTopicConnectionFactory());
-			topicConnection = topicConnectionFactory.createTopicConnection();
-			topic = (Topic) ic.lookup("jms/ICAT/Topic");
-			logger.info("Notification Transmitter created");
-		} catch (JMSException | NamingException e) {
-			logger.error(fatal, "Problem with JMS " + e);
-			throw new IllegalStateException(e.getMessage());
-		}
-
-	}
-
-	@PreDestroy()
-	void exit() {
-		try {
-			if (topicConnection != null) {
-				topicConnection.close();
-			}
-			logger.info("Notification Transmitter closing down");
-		} catch (JMSException e) {
-			throw new IllegalStateException(e.getMessage());
-		}
-	}
+	@Resource(lookup="jms/ICAT/Topic")
+	Topic topic;
 
 	public void processMessage(NotificationMessage notificationMessage) throws JMSException {
 		Message message = notificationMessage.getMessage();
 		if (message != null) {
-			try ( Session jmsSession = topicConnection.createSession(false, Session.AUTO_ACKNOWLEDGE) )
-			{
-				MessageProducer jmsProducer = jmsSession.createProducer(topic);
-				ObjectMessage jmsg = jmsSession.createObjectMessage();
+			try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+				ObjectMessage jmsg = context.createObjectMessage();
 				jmsg.setStringProperty("entity", message.getEntityName());
 				jmsg.setStringProperty("operation", message.getOperation());
 				jmsg.setObject(message.getEntityId());
-				jmsProducer.send(jmsg);
-				logger.debug("Sent jms notification message " + message.getOperation() + " " + message.getEntityName() + " "
-					+ message.getEntityId());
+
+				context.createProducer().send(topic, jmsg);
+				logger.debug("Sent jms notification message " + message.getOperation() + " " + message.getEntityName() + " " + message.getEntityId());
 			} catch (JMSException e) {
 				logger.error("Failed to send jms notification message ");
 			}
 		}
-
 	}
 }
--- src/main/java/org/icatproject/core/manager/Transmitter.java
+++ src/main/java/org/icatproject/core/manager/Transmitter.java
@@ -1,80 +1,39 @@
 package org.icatproject.core.manager;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
 import jakarta.jms.JMSException;
-import jakarta.jms.MessageProducer;
-import jakarta.jms.Session;
 import jakarta.jms.TextMessage;
 import jakarta.jms.Topic;
-import jakarta.jms.TopicConnection;
-import jakarta.jms.TopicConnectionFactory;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
 @ApplicationScoped
 public class Transmitter {
 
-	private static Logger logger = LoggerFactory.getLogger(Transmitter.class);
-	private final static Marker fatal = MarkerFactory.getMarker("FATAL");
+	private static final Logger logger = LoggerFactory.getLogger(Transmitter.class);
 
-	private Topic topic;
+	@Resource
+	ConnectionFactory connectionFactory;
 
-	private TopicConnection topicConnection;
-
-	@Inject
-	PropertyHandler propertyHandler;
-
-	@PostConstruct
-	void init() {
-
-		try {
-			InitialContext ic = new InitialContext();
-			TopicConnectionFactory topicConnectionFactory = (TopicConnectionFactory) ic
-					.lookup(propertyHandler.getJmsTopicConnectionFactory());
-			topicConnection = topicConnectionFactory.createTopicConnection();
-			topic = (Topic) ic.lookup("jms/ICAT/log");
-			logger.info("Transmitter created");
-		} catch (JMSException | NamingException e) {
-			logger.error(fatal, "Problem with JMS " + e);
-			throw new IllegalStateException(e.getMessage());
-		}
-
-	}
-
-	@PreDestroy()
-	void exit() {
-		try {
-			if (topicConnection != null) {
-				topicConnection.close();
-			}
-			logger.info("Transmitter closing down");
-		} catch (JMSException e) {
-			throw new IllegalStateException(e.getMessage());
-		}
-	}
+	@Resource(lookup="jms/ICAT/log")
+	Topic topic;
 
 	public void processMessage(String operation, String ip, String body, long startMillis) {
-		try ( Session jmsSession = topicConnection.createSession(false, Session.AUTO_ACKNOWLEDGE) )
-		{
-			TextMessage jmsg = jmsSession.createTextMessage(body);
+		try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+			TextMessage jmsg = context.createTextMessage(body);
 			jmsg.setStringProperty("operation", operation);
 			jmsg.setStringProperty("ip", ip);
 			jmsg.setLongProperty("millis", System.currentTimeMillis() - startMillis);
 			jmsg.setLongProperty("start", startMillis);
-			MessageProducer jmsProducer = jmsSession.createProducer(topic);
-			jmsProducer.send(jmsg);
+
+			context.createProducer().send(topic, jmsg);
 			logger.debug("Sent jms message " + operation + " " + ip);
 		} catch (JMSException e) {
 			logger.error("Failed to send jms message " + operation + " " + ip);
 		}
 	}
-
 }
